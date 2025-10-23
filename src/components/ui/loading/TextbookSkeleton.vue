349a61ad33035cff5f4848e771c9b1a81fbe89ef
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2025-10-23 16:47:52 +0300

    added textbook-item skeleton

--- src/components/ui/loading/TextbookSkeleton.vue
+++ src/components/ui/loading/TextbookSkeleton.vue
@@ -4,6 +4,14 @@
 
 <template>
 
-  загрузка учебника...
+  <div class="skeleton skeleton-default-item">
+    <div class="skeleton-default-item__title line line__background h-30"></div>
+    <div class="skeleton-default-item__slider flex">
+      <div class="line__background" v-for="_ in 3"></div>
+    </div>
+    <div class="skeleton-default-item__text line line__background h-20" v-for="_ in 5"></div>
+    <div class="skeleton-default-item__code line line__background"></div>
+    <div class="skeleton-default-item__text line line__background h-20" v-for="_ in 3"></div>
+  </div>
 
 </template>
